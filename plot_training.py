b5d8160680d239dd4346d8bd4cc4340deff43d3a
ethan-r-gallup <ethanrobertgallup@gmail.com>
2022-02-27 23:55:21 -0700

    collect transfer learning data

--- plot_training.py
+++ plot_training.py
@@ -22,8 +22,9 @@ phy = np.array([])
 base = np.array([])
 hybrid = np.array([])
 transfer = np.array([])
+transfer2 = np.array([])
 cntrl = np.array([])
-i = 0
+
 for col, hcol in zip(baseline.columns, hlen):
     p = pd.read_csv(f'performance data/phyloss_perf_{col}4.csv')
     # print(p)
@@ -31,21 +32,26 @@ for col, hcol in zip(baseline.columns, hlen):
     h = pd.read_csv(f'performance data/hybrid_perf_{col}.csv')
     # print(h)
     hme = np.mean((h.pred - h.Cc)**2)
-    i += 1
     t = pd.read_csv(f'performance data/transfer_perf_{col}.csv')
     # print(t)
     tme = np.mean((t.Cc_pred - t.Cc)**2)
+    t2 = pd.read_csv(f'performance data/transfer_perf_{col}_2.csv')
+    # print(t2)
+    tme2 = np.mean((t2.Cc_pred - t2.Cc)**2)
     b = pd.read_csv(f'performance data/baseline_perf_{col}_2.csv')
-
     bme = np.mean(abs(b.Cc_pred - b.Cc)**2)
+
     phy = np.append(phy, pme)
     base = np.append(base, bme)
     hybrid = np.append(hybrid, hme)
     transfer = np.append(transfer, tme)
+    transfer2 = np.append(transfer2, tme2)
 
 plt.plot(base, label='base')
 plt.plot(phy, label='phy loss')
 plt.plot(hybrid, label='hybrid')
+# plt.plot(transfer, label='transfer')
+plt.plot(transfer2, label='transfer2')
 
 # plt.plot(transfer, label='transfer')
 plt.legend()
