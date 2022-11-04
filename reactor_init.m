95575f50a77208252d73b1272c7cd3d57a20fbd7
ethan-r-gallup <ethanrobertgallup@gmail.com>
2022-11-04 14:30:08 -0600

    add simulation files

---
+++ reactor_init.m
@@ -0,0 +1,8 @@
+d = 4; %m, reactor diameter
+Cvalve = 3.2; %m^2.5/min, reactor valve constant
+ko = .23; %m^3/kmol*min, rate constant
+Ea = 9500; %kJ/kmol, reaction activation energy
+R = 8.314; %kJ/kmol*K, ideas gas constant
+rho = 780; %kh/m^3, density of all liquids
+Cp = 3.25; %kJ/kg*K, Heat capacity of all liquids
+dH = 156000; %kJ/kmol, heat of formation (per mole of A consumed)
